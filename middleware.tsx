97dad9353619d01bd0d09da1c4496843be643f85
heykapil <kapilchaudhary@gujaratuniversity.ac.in>
2025-11-15 00:11:07 +0530

    using headers instead of req.nextUrl

--- middleware.tsx
+++ middleware.tsx
@@ -8,24 +8,46 @@ export async function middleware(request: NextRequest) {
   const production = process.env.NODE_ENV === 'production';
   const secureCookie: boolean = production;
   const cookiePrefix = secureCookie ? '__Secure-' : '';
-  const session_token = request.cookies.get(`${cookiePrefix}kapil.app.session_token`)?.value || '';
-  const sessionData = request.cookies.get(`${cookiePrefix}kapil.app.sessionData`)?.value || '';
+  const session_token =
+    request.cookies.get(`${cookiePrefix}kapil.app.session_token`)?.value || '';
+  const sessionData =
+    request.cookies.get(`${cookiePrefix}kapil.app.sessionData`)?.value || '';
+  const protocol = request.headers.get('x-forwarded-proto') || 'http';
+  const host = request.headers.get('x-forwarded-host');
+  const publicCallbackUrl = new URL(
+    request.nextUrl.pathname + request.nextUrl.search,
+    `${protocol}://${host}`,
+  );
   const midResponse = NextResponse.next();
 
   if (production && !session_token) {
-    return NextResponse.redirect(new URL('/login?redirectTo=' + encodeURIComponent(request.nextUrl as unknown as string), process.env.BETTER_AUTH_URL!));
+    return NextResponse.redirect(
+      new URL(
+        '/login?redirectTo=' + encodeURIComponent(publicCallbackUrl.toString()),
+        process.env.BETTER_AUTH_URL!,
+      ),
+    );
   }
 
   if (production && session_token && !sessionData) {
-    const { data: session } = await betterFetch<Session>('/api/auth/get-session', {
-      baseURL: process.env.BETTER_AUTH_URL,
-      headers: {
-        cookie: request.headers.get("cookie") || "",
+    const { data: session } = await betterFetch<Session>(
+      '/api/auth/get-session',
+      {
+        baseURL: process.env.BETTER_AUTH_URL,
+        headers: {
+          cookie: request.headers.get('cookie') || '',
+        },
       },
-    });
+    );
 
     if (production && !session) {
-      return NextResponse.redirect(new URL('/login?redirectTo=' + encodeURIComponent(request.nextUrl as unknown as string), process.env.BETTER_AUTH_URL!));
+      return NextResponse.redirect(
+        new URL(
+          '/login?redirectTo=' +
+            encodeURIComponent(request.nextUrl as unknown as string),
+          process.env.BETTER_AUTH_URL!,
+        ),
+      );
     }
 
     if (production) {
@@ -34,7 +56,8 @@ export async function middleware(request: NextRequest) {
         value: await signJWT(session!),
         httpOnly: true,
         sameSite: 'lax',
-        domain: process.env.NODE_ENV === 'production' ? '.kapil.app' : '.localhost',
+        domain:
+          process.env.NODE_ENV === 'production' ? '.kapil.app' : '.localhost',
         secure: process.env.NODE_ENV === 'production' ? true : false,
         expires: new Date(Date.now() + 1000 * 60 * 5),
         path: '/',
